dd655a98ed43007521d269fa5ec7d21b629e985d
Kevin Saldana <ksaldana1@gmail.com>
2024-07-19 16:33:31 -0400

    sidebar stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import { Canvas } from "@react-three/fiber";
 import clsx from "clsx";
-import { Scene } from "./components/Scene";
-import { useState } from "react";
 import { animated, useSpring } from "react-spring";
+import { Scene } from "./components/Scene";
 import { AppProvider } from "./context/AppProvider";
+import { useAppContext } from "./context/app";
+import { Embedding } from "./common/types";
 
 function App() {
   return (
@@ -21,18 +22,34 @@ function App() {
 }
 
 function Sidebar() {
-  const [open, setOpen] = useState(false);
+  const { state } = useAppContext();
+  const isOpen = state.selected;
   const { height } = useSpring({
-    height: open ? "75%" : "0%",
+    height: isOpen ? "75%" : "0%",
   });
   return (
     <animated.div
-      onClick={() => setOpen((s) => !s)}
       style={{ height }}
-      className={clsx("absolute w-96 bg-green-500 left-12")}
+      className={clsx(
+        "absolute w-96  bg-blue-100 opacity-75 left-12 rounded-md"
+      )}
     >
-      {open ? "Sidebar" : null}
+      {isOpen && state.selected ? <Content embedding={state.selected} /> : null}
     </animated.div>
+  );
+}
+
+function Content({ embedding }: { embedding: Embedding }) {
+  return (
+    <ul className="flex flex-col p-5 gap-3">
+      <li>ID: {embedding.id}</li>
+      <li>
+        UMAP:{" "}
+        {embedding.umap.map((x) => (
+          <span className="mx-4">{x.toFixed(2)}</span>
+        ))}
+      </li>
+    </ul>
   );
 }
 
