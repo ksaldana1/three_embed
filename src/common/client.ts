ea37e9071594e9dbb54c9800cd3c24e384ecae60
Kevin Saldana <ksaldana1@gmail.com>
2024-07-20 13:37:14 -0400

    .

--- src/common/client.ts
+++ src/common/client.ts
@@ -1,8 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 import { Database } from "../generated/database.types";
 
-console.log(import.meta.env.VITE_SUPABASE_KEY);
-
 export const client = createClient<Database>(
   "https://ndxvdeqzmhnxzagdxytn.supabase.co",
   import.meta.env.VITE_SUPABASE_KEY
