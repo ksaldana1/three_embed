ba55678e8ae4dc05be2b8bfd26b0b1fab073be12
Kevin Saldana <ksaldana1@gmail.com>
2024-07-20 14:26:39 -0400

    stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import { Canvas } from "@react-three/fiber";
 import clsx from "clsx";
+import { useEffect, useState } from "react";
 import { animated, useSpring } from "react-spring";
-import { Embedding } from "./common/types";
+import { client } from "./common/client";
 import { Scene } from "./components/Scene";
 import { AppProvider } from "./context/AppProvider";
 import { useAppContext } from "./context/app";
@@ -21,8 +22,31 @@ function App() {
   );
 }
 
+type PromiseReturnType<T> = T extends Promise<infer R> ? R : never;
+
+async function fetchSelectedEmbedding(id: string) {
+  const { data } = await client
+    .from("movies")
+    .select("imdbID, Title, Director")
+    .eq("imdbID", id);
+  return data![0];
+}
+
+type SelectedType = PromiseReturnType<
+  ReturnType<typeof fetchSelectedEmbedding>
+>;
+
 function Sidebar() {
   const { state } = useAppContext();
+  const [selectedData, setSelectedData] = useState<SelectedType | null>(null);
+
+  useEffect(() => {
+    if (state?.selected?.id) {
+      fetchSelectedEmbedding(state.selected.id).then((data) =>
+        setSelectedData(data)
+      );
+    }
+  }, [state?.selected?.id]);
   const isOpen = state.selected;
   const { height } = useSpring({
     height: isOpen ? "75%" : "0%",
@@ -34,21 +58,17 @@ function Sidebar() {
         "absolute w-96 bg-blue-100 opacity-75 left-12 rounded-md"
       )}
     >
-      {isOpen && state.selected ? <Content embedding={state.selected} /> : null}
+      {isOpen && selectedData ? <Content embedding={selectedData} /> : null}
     </animated.div>
   );
 }
 
-function Content({ embedding }: { embedding: Embedding }) {
+function Content({ embedding }: { embedding: SelectedType }) {
   return (
     <ul className="flex flex-col p-5 gap-3">
-      <li>ID: {embedding.id}</li>
-      <li>
-        UMAP:{" "}
-        {embedding.umap.map((x) => (
-          <span className="mx-4">{x.toFixed(2)}</span>
-        ))}
-      </li>
+      <li>ID: {embedding.imdbID}</li>
+      <li>Title: {embedding.Title}</li>
+      <li>Director: {embedding.Director}</li>
     </ul>
   );
 }
